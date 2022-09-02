d06c1fda9e1cf625a0bdafa62361e2678c8ed684
Rafał Gołubowicz <rafalgolubowicz@MacBook-Pro-Rafa.local>
2022-09-02 17:33:31 +0200

    setup swagger

--- src/api/healthCheck/router.ts
+++ src/api/healthCheck/router.ts
@@ -5,6 +5,17 @@ import { getSystemStatus } from "./controller";
 
 const healthCheckRouter = Router();
 
-healthCheckRouter.use(HealthCheckEndpoints.STATUS, getSystemStatus);
+/**
+ * @openapi
+ * /status:
+ *  get:
+ *     tags:
+ *     - Healthcheck
+ *     description: Responds if the app is up and running
+ *     responses:
+ *       200:
+ *         description: App is up and running
+ */
+healthCheckRouter.get(HealthCheckEndpoints.STATUS, getSystemStatus);
 
 export default healthCheckRouter;
--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,18 @@
 import normalizePort from "./utils/normalizePort";
 import { SERVER_PORT } from "./setup/environment";
 import createExpressApp from "./utils/createExpressApp";
+import setupSwagger from "./utils/setupSwagger";
 
 const app = createExpressApp();
 
 const port = normalizePort(SERVER_PORT);
 
 // Start server
-app.listen(port, () =>
-  console.log(`Server is listening on port ${SERVER_PORT}!`)
-);
+app.listen(port, () => {
+  console.log(`Server is listening on port ${SERVER_PORT}!`);
+});
+
+// Start Swagger
+setupSwagger(app, "/swagger", () => {
+  console.log(`Docs available at http://localhost:${port}/swagger`);
+});
